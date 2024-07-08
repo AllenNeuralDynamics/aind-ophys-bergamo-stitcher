1531b4212f0f0cd468726c5a5a38220f60eb2ab5
Arielle Leon <ariellel@alleninstitute.org>
2024-07-08 12:19:19 -0700

    formatting, adding build file and logging

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,12 @@
 """ top level run script """
 
+from logging_config import setup_logging
+
+
 def run():
-    """ basic run function """
+    """basic run function"""
     pass
 
-if __name__ == "__main__": run()
+
+if __name__ == "__main__":
+    setup_logging("/results/aind_ophys_bergamo_stitcher.log")
