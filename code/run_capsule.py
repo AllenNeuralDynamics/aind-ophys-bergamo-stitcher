152e5924cf0f11db1c1dd179a0f7f0013a61b588
Arielle Leon <ariellel@alleninstitute.org>
2024-08-18 13:46:07 -0700

    adding run file

---
+++ code/run_capsule.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from bergamo_stitcher import BergamoSettings, BergamoTiffStitcher
+import json
+from logging_config import setup_logging
+
+
+if __name__ == "__main__":
+    setup_logging("..//results/aind_ophys_bergamo_stitcher.log")
+    input_dir = next(Path("../data").glob("*/ophys"))
+    output_dir = Path("../results")
+    unique_id = next(Path("../data").glob("single-plane*"))
+    unique_id = "_".join(str(unique_id).split("_")[-3:])
+    bergamo_settings = BergamoSettings(input_dir = input_dir, output_dir=output_dir, unique_id = unique_id)
+    bergamo_stitcher = BergamoTiffStitcher(bergamo_settings)
+    bergamo_stitcher.run_converter()
