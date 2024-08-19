91bdf697a818a0cee631b16502eca025f39d84d5
Arielle Leon <ariellel@alleninstitute.org>
2024-08-19 01:15:30 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -5,9 +5,9 @@ from logging_config import setup_logging
 
 
 if __name__ == "__main__":
-    setup_logging("..//results/aind_ophys_bergamo_stitcher.log")
+    setup_logging("../results/aind_ophys_bergamo_stitcher.log")
     input_dir = Path("../data/")
-    data_dir = next(input_dir.glob("*/ophys"))
+    data_dir = next(input_dir.glob("/*/ophys"))
     output_dir = Path("../results")
     unique_id = next(Path("../data").glob("single-plane*"))
     unique_id = "_".join(str(unique_id).split("_")[-3:])
