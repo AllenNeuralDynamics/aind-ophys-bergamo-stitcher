60cb0f94d9128fa7b74d7e4b0ca51bbd51bca758
Arielle Leon <ariellel@alleninstitute.org>
2024-08-19 17:11:11 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -7,10 +7,14 @@ from logging_config import setup_logging
 if __name__ == "__main__":
     setup_logging("../results/aind_ophys_bergamo_stitcher.log")
     input_dir = Path("../data/")
-    print(f"{list(input_dir.glob('*'))}")
-    data_dir = next(input_dir.glob("*/ophys"))
+    try:
+        data_dir = next(input_dir.glob("*/ophys"))
+    except:
+        data_dir = next(input_dir.glob("*ophys"))
     output_dir = Path("../results")
-    unique_id = next(Path("../data").glob("single-plane*"))
+    data_description = next(input_dir.rglob("data_description.json")
+    with open(data_description) as j:
+        unique_id = json.load(j)["name"]
     unique_id = "_".join(str(unique_id).split("_")[-3:])
     bergamo_settings = BergamoSettings(input_dir = data_dir, output_dir=output_dir, unique_id = unique_id)
     bergamo_stitcher = BergamoTiffStitcher(bergamo_settings)
